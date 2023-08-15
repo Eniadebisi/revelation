62ed8d3b44bc6c41599cae6451fa8c08d0dceff5
Eniadebisi <eniadebisi@gmail.com>
2023-08-15 07:46:30 -0400

    Base setup w Nodemon

--- server-local.js
+++ server-local.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const app = require('./express/server');
+const app = require('./server');
+
+app.set('view engine', 'ejs');
+
+app.get("/bye", (req, res) => {
+    res.render(index);
+})
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
